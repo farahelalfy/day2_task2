4ce9d57eb0a5cbaaf6a6e9e08ba71dae27413dc5
farah <farah@LAPTOP-RJCB7SM0>
2024-07-11 14:20:51 +0300

    Add project files.

---
+++ day 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("plz enter your name ");
+           string name=Console.ReadLine();
+            Console.WriteLine("plz enter your id: ");
+            int id=int.Parse(Console.ReadLine());
+            int age;
+            do
+            {
+                Console.WriteLine("plz enter your age:");
+                age = int.Parse(Console.ReadLine());
+            }while( age <1 );
+            Console.WriteLine($"""
+              your name is: {name} 
+
+              your id is: {id}
+
+              your age is:{age}
+              """);
+        }
+    }
+}
